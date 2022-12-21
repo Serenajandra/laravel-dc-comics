a318ee677e0154e99c4f86f78d049911a4a0bc7c
Serenajandra <serena.jandra@gmail.com>
2022-12-21 18:01:09 +0100

    creata tabella in index

---
+++ resources/views/Comics/index.blade.php
@@ -0,0 +1,36 @@
+@extends('Layouts.app')
+@section('title', 'Tutti i comics')
+@section('content')
+    <div class="container">
+        <h2 class="text-center">
+            Tutti i nostri Comics
+        </h2>
+        <table class="table table-striped">
+            <thead>
+
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Serie</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Dettagli</th>
+
+                </tr>
+            </thead>
+            <tbody>
+
+
+                @foreach ($Comics as $comic)
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td> <a href="">Link</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+@endsection
